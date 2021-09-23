42c726a650e94f3e72296e1eba6c4b0f10920bd7
Will Gray <graywh@gmail.com>
2021-09-23 07:54:17 -0500

    Update README and tests for change from xip.to to nip.io

--- test/praxrc_test.rb
+++ test/praxrc_test.rb
@@ -3,6 +3,6 @@ require_relative "test_helper"
 class PraxrcTest < Minitest::Test
   def test_praxrc_loaded
     assert_equal "itworks", Net::HTTP.get(URI("http://praxrc.localhost:20557/"))
-    assert_equal "itworks", Net::HTTP.get(URI("http://praxrc.127.0.0.1.xip.io:20557/"))
+    assert_equal "itworks", Net::HTTP.get(URI("http://praxrc.127.0.0.1.nip.io:20557/"))
   end
 end
